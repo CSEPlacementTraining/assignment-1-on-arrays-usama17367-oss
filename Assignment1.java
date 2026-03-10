b6b286bc296a38737a73de3dbe6d6e663385aafb
usama17367-oss <usama17367@gmail.com>
2026-03-10 08:01:28 +0000

    Assignment1 on Topic to find the next 15 leap years

--- Assignment1.java
+++ Assignment1.java
@@ -1,16 +1,36 @@
 class Assignment1 {
   public static int[] findLeapYears(int year) {
-  // Implement your code here and change the return value accordingly
-    
-  return null;
+    int[] leapYears = new int[15];
+    int count = 0;
+    int currentYear = year;
+
+    while (count < 15) {
+      if (isLeapYear(currentYear)) {
+        leapYears[count] = currentYear;
+        count++;
+      }
+      currentYear++;
+    }
+    return leapYears;
   }
-  
+
+  private static boolean isLeapYear(int year) {
+    if (year % 400 == 0) {
+      return true;
+    } else if (year % 4 == 0 && year % 100 != 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   public static void main(String[] args) {
     int year = 2000;
     int[] leapYears;
     leapYears = findLeapYears(year);
     for (int index = 0; index < leapYears.length; index++) {
-      System.out.println(leapYears[index]);
+      System.out.print(leapYears[index] + " ");
     }
+    System.out.println(); // For a new line after output
   }
 }
